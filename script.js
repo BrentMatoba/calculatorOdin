f358f3604eae87013731a5dc444c31bd9ef921d9
BrentMatoba <118773387+BrentMatoba@users.noreply.github.com>
2023-10-13 11:25:48 -0700

    add display functionality, successfully test button functionality

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 let num1;
 let num2;
 let operator;
+let display = document.getElementById("screenText");
 
 
 
@@ -20,7 +21,7 @@ function divide(num1, num2){
 
 function operate(num1, num2, operator){
     if(operator == "+"){
-        add(num1, num2);
+        console.log(add(num1, num2));
     }
     else if(operator == "-"){
         subtract(num1, num2);
@@ -32,16 +33,35 @@ function operate(num1, num2, operator){
         divide(num1, num2);
     }
     else{console.log("Invalid operator");
+        return 0;
+    }}
+
+function updateDisplayNumber(){
+    //get number from button
+    number = this.textContent;
+    if(display.textContent == 0){
+        display.innerHTML = number;  
     }
+    else{
+        display.textContent += number;
+    }
+    //update display
+    
+}
+function updateDisplayOperator(){
+
 }
 
 
+let button = document.getElementById("seven");
+button.addEventListener("click", updateDisplayNumber); //remember not to put () on the function, makes it act immediately instead of waiting for the click
 
 
+    //hitting equals calls operate function on num1, num2, operator.
+    //how do i handle multiple operators?
+    //how do i display?
+    //make buttons able to show on display
+        //numbers in display probably stored as strings so that I can += to add numbers next to each other, then converted to int upon calculation.
+        //each button should += a target
 
 
-for(let i=0; i < 4; i++){
-    let numpad = document.getElementById("numpad");
-    document.createElement("div");
-
-}
